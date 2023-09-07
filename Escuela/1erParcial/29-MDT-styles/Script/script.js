985f35732d2fe4d62588606f70cf033105c5ff3f
Eri <103050349+Eriarer@users.noreply.github.com>
2023-09-06 21:34:55 -0600

    Slide show

--- Escuela/1erParcial/29-MDT-styles/Script/script.js
+++ Escuela/1erParcial/29-MDT-styles/Script/script.js
@@ -1,3 +1,5 @@
+// ZOOM DE IMAGENES
+
 function imageZoom(imgID, resultID) {
   var img, lens, result, cx, cy;
   img = document.getElementById(imgID);
@@ -71,6 +73,8 @@ function imageZoom(imgID, resultID) {
     return { x: x, y: y };
   }
 }
+
+// COMPARACION DE IMAGENES
 
 function initComparisons() {
   var x, i;
@@ -152,4 +156,40 @@ function initComparisons() {
       slider.style.left = img.offsetWidth - slider.offsetWidth / 2 + "px";
     }
   }
+}
+
+// SLIDES
+
+let slideIndex = 1;
+function initSlides() {
+  showSlides(slideIndex);
+}
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
