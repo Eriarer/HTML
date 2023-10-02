8da05607cadfe906d449245bb992622f124395d0
Eriarer <eriarer_melgoza+git@hotmail.com>
2023-10-02 09:30:22 -0600

    01php

---
+++ xampp/SegundoParcial/01-MDT-holaMundo.php
@@ -0,0 +1,10 @@
+<h1>Mi primer programa con php</h1>
+<?php
+print("Hola Mundo");
+echo "<br>Hola Mundo";
+$nombre = "Abraham";
+$sem = "5to";
+echo "<h1> Hola Mundo" . $nombre . "ISC </h1>";
+$texto = "<h1> Hola Mundo" . $nombre . "ISC $sem </h1>";
+echo $texto;
+?>
