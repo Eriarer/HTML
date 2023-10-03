c2443f8716b6fb791aa56140abc88295b555b9df
Eriarer <eriarer_melgoza+git@hotmail.com>
2023-10-03 07:54:06 -0600

    php formatter

--- xampp/SegundoParcial/02-MDT-php.php
+++ xampp/SegundoParcial/02-MDT-php.php
@@ -11,45 +11,46 @@
   <h1>Programa#2</h1>
   <h2>Melgoza de la Torre Abraham</h2>
   <?php
-    //  comentario
-    // comentario
-    /*
-     * comentario
-     * comentario
-     */
-    print ('Hola Mundo');
-    echo '<h3> Hola persona <h3>';
+//  comentario
+// comentario
+/*
+ * comentario
+ * comentario
+ */
+print('Hola Mundo');
+echo '<h3> Hola persona <h3>';
 
-    $carrera = 'Inge en Sistemas';
-    $texto = "<p>Estudiante es un $carrera </p>";
-    echo $texto;
-    $texto = '<p>Estudiante es el ' . $carrera . '</p>';
-    echo $texto;
+$carrera = 'Inge en Sistemas';
+$texto = "<p>Estudiante es un $carrera </p>";
+echo $texto;
+$texto = '<p>Estudiante es el ' . $carrera . '</p>';
+echo $texto;
 
-    if (isset($_GET['base']) && isset($_GET['altura'])) {
-      $base = $_GET['base'];
-      $altura = $_GET['altura'];
-      echo "Base= $base <br>";
-      echo "Altura= $altura <br>";
-      $area = calcularArea($base, $altura);
-      echo "<h3 style= 'background:green;color:black;'>El area del rectangulo es $area</h3>";
-    } else
-      echo "<h3 style= 'background:red;color:black;'>No hay variables par calcular el rectangulo</h3>";
+if (isset($_GET['base']) && isset($_GET['altura'])) {
+    $base = $_GET['base'];
+    $altura = $_GET['altura'];
+    echo "Base= $base <br>";
+    echo "Altura= $altura <br>";
+    $area = calcularArea($base, $altura);
+    echo "<h3 style= 'background:green;color:black;'>El area del rectangulo es $area</h3>";
+} else {
+    echo "<h3 style= 'background:red;color:black;'>No hay variables par calcular el rectangulo</h3>";
+}
 
-    $personas = ['Miguel', 'Angel', 'Augusto', 'Heriberto'];
-    echo "<h4> $personas[0] $personas[1] $personas[2] $personas[3] $personas[2] </h4>";
+$personas = ['Miguel', 'Angel', 'Augusto', 'Heriberto'];
+echo "<h4> $personas[0] $personas[1] $personas[2] $personas[3] $personas[2] </h4>";
 
-    function calcularArea($b, $h)
-    {
-      return $b * $h;
-    }
-  ?>
+function calcularArea($b, $h)
+{
+    return $b * $h;
+}
+?>
   <h1>Listado de nombrerijillos</h1>
   <ul>
     <?php
-      foreach ($personas as $persona) {
-        echo "<li>$persona</li>";
-      }
-    ?>
+foreach ($personas as $persona) {
+    echo "<li>$persona</li>";
+}
+?>
   </ul>
 </body>
