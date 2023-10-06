e0fe2edf64b6042b4fc73b388e7941eba7c8ec77
Eri <103050349+Eriarer@users.noreply.github.com>
2023-10-05 19:46:57 -0600

    web responsivnes on xl screens is weird

--- xampp/SegundoParcial/16-MDT-hotel/php/form.php
+++ xampp/SegundoParcial/16-MDT-hotel/php/form.php
@@ -84,6 +84,8 @@ $precioTotal = $precio - $descuento;
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Las Glorias</title>
+  <!-- cache -->
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous" />
   <link rel="stylesheet" href="../styles/style.css">
   <link rel="stylesheet" href="../styles/output.css">
