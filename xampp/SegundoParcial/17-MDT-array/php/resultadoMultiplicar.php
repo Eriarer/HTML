a33a6d830cea7d4747937395ecd4f5674f8c3e04
Eri <103050349+Eriarer@users.noreply.github.com>
2023-10-07 10:27:29 -0600

    wide problem solved for mobile

--- xampp/SegundoParcial/17-MDT-array/php/resultadoMultiplicar.php
+++ xampp/SegundoParcial/17-MDT-array/php/resultadoMultiplicar.php
@@ -76,6 +76,7 @@
     </div>
   </div>
   <div class="contenedor">
+    <button class="btn btn-block" onclick="regresarAlMenu('../html/multiplicar.html')">Regresar</button>
     <table class="table table-striped">
       <thead>
         <tr>
@@ -126,14 +127,11 @@
       </tbody>
     </table>
     <hr>
-    <button class="btn " onclick="regresarAlMenu('../html/multiplicar.html')">Regresar</button>
+    <button class="btn btn-block" onclick="regresarAlMenu('../html/multiplicar.html')">Regresar</button>
   </div>
 </body>
 <!-- scripts -->
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
-<script>
-  mostrarModal();
-</script>
 
 </html>
