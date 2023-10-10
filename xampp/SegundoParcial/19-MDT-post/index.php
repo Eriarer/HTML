32aea942a37fb389031c24e4c3eb01d1c961c1a5
Eriarer <eriarer_melgoza+git@hotmail.com>
2023-10-10 16:11:26 -0600

    finished

--- xampp/SegundoParcial/19-MDT-post/index.php
+++ xampp/SegundoParcial/19-MDT-post/index.php
@@ -26,39 +26,108 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <div class="header text-center">
       <h1 class="h3">Melgoza de la Torre Abraham</h1>
     </div>
-    <div class="form">
-      <!-- form de boostrap -->
-      <!-- metodo post -->
-      <!-- pide el tamaño de un vector
-      min y max -->
-      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <div class="form-row">
-          <div class="form-group col">
-            <label for="arrSize">Tamaño vector: <span id="sliderValue">0</span></label>
-            <div class="sliderContainer">
-              <p>0</p>
-              <input type="range" class="form-control-range" name="arrSize" id="slider" min="0" max="100" value="50">
-              <p>100</p>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    ?>
+      <div class="form">
+
+        <!-- form de boostrap -->
+        <!-- metodo post -->
+        <!-- pide el tamaño de un vector
+min y max -->
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+          <div class="form-row">
+            <div class="form-group col">
+              <label for="arrSize" class="h2">Tamaño vector: <span id="sliderValue" class="h2">0</span></label>
+              <div class="sliderContainer" id="slideContainer">
+                <h3>0</h3>
+                <input type="range" class="form-control-range" name="arrSize" id="slider" min="0" max="100" value="30">
+                <h3>100</h3>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="row mb-2">
-          <div class="col">
-            <input type="number" class="form-control form-control-lg" placeholder="N menor" aria-label="Numero menor" name="min" id="min" required />
+          <div class="row mb-2">
+            <div class="col">
+              <input type="number" class="form-control form-control-lg" placeholder="N menor" aria-label="Numero menor" name="min" id="min" required />
+            </div>
+            <div class="col">
+              <input type="number" class="form-control form-control-lg" placeholder="N mayor" aria-label="Numero mayor" name="max" id="max" required />
+            </div>
           </div>
-          <div class="col">
-            <input type="number" class="form-control form-control-lg" placeholder="N mayor" aria-label="Numero mayor" name="max" id="max" required />
+          <div class="row">
+            <div class="col">
+              <button type="submit" class="btn btn-info btn-lg btn-block font-weight-bold">
+                Calcular!
+              </button>
+            </div>
           </div>
+        </form>
+      </div>
+    <?php
+    } else {
+      // generar un arreglo de tamaño arrSize 
+      // con numeros aleatorios entre min y max sin repetir
+      $arr = array();
+      $i = 0;
+      while ($i < $arrSize) {
+        $num = rand($min, $max);
+        if (!in_array($num, $arr)) {
+          $arr[$i] = $num;
+          $i++;
+        }
+      }
+      // copiar el $arr y ordenarlo
+      // de menor a mayor
+      // de mayor a menor
+      // obtener el mayor y el menor
+      $arrCopy = $arr;
+      sort($arrCopy);
+      $arrCopy2 = $arrCopy;
+      rsort($arrCopy2);
+      $min = $arrCopy[0];
+      $max = $arrCopy2[0];
+    ?>
+      <div class="vector">
+        <h2>Vector</h2>
+        <div class='vectorItem'>
+          <?php
+          foreach ($arr as $key => $value) {
+            echo "<p>$value</p>";
+          }
+          ?>
         </div>
-        <div class="row">
-          <div class="col">
-            <button type="submit" class="btn btn-lg btn-block font-weight-bold">
-              Calcular!
-            </button>
-          </div>
+      </div>
+      <!-- vector ordenado -->
+      <div class="vector">
+        <h2>Vector de Menor a Mayor</h2>
+        <div class='vectorItem'>
+          <?php
+          foreach ($arrCopy as $key => $value) {
+            echo "<p>$value</p>";
+          }
+          ?>
         </div>
-      </form>
-    </div>
+      </div>
+      <!-- vector ordenado 2 -->
+      <div class="vector">
+        <h2>Vector de Mayor a Menor</h2>
+        <div class='vectorItem'>
+          <?php
+          foreach ($arrCopy2 as $key => $value) {
+            echo "<p>$value</p>";
+          }
+          ?>
+        </div>
+      </div>
+      <!-- mayor y menor -->
+      <div class="vector">
+        <h2>Menor y Mayor</h2>
+        <div class='vectorItem'>
+          <p>Menor: <?php echo $min; ?></p>
+          <p>Mayor: <?php echo $max; ?></p>
+        </div>
+      </div>
+    <?php  } ?>
   </div>
 </body>
 <!-- js -->
--- xampp/SegundoParcial/19-MDT-post/script.js
+++ xampp/SegundoParcial/19-MDT-post/script.js
@@ -1,12 +1,11 @@
 const slider = document.getElementById("slider");
 const sliderValue = document.getElementById("sliderValue");
+var formulario = document.querySelector("form");
 
 sliderValue.textContent = slider.value;
 slider.addEventListener("input", () => {
   sliderValue.textContent = slider.value;
 });
-
-var formulario = document.querySelector("form");
 
 document.addEventListener("DOMContentLoaded", function () {
   var formulario = document.querySelector("form");
@@ -16,11 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
   function validar(e) {
     var minimo = document.querySelector("#min");
     var maximo = document.querySelector("#max");
+    var slider = document.querySelector("#slider");
     var min = minimo.value;
     min = parseInt(min);
     var max = maximo.value;
     max = parseInt(max);
-
+    var slide = slider.value;
+    slide = parseInt(slide);
+    var dif = max - min;
     // Obtener el div existente que muestra el mensaje de error
     var mensajeErrorDiv = document.querySelector(
       ".invalid-feedback"
@@ -41,6 +43,24 @@ document.addEventListener("DOMContentLoaded", function () {
         "El valor mínimo no puede ser mayor al valor máximo";
 
       // Agregar el nuevo div de error después del campo "min"
+      minimo.parentNode.insertBefore(
+        mensajeError,
+        minimo.nextSibling
+      );
+
+      // Agregar la clase "is-invalid" a los campos "min" y "max" para resaltarlos como inválidos
+      minimo.classList.add("is-invalid");
+      maximo.classList.add("is-invalid");
+    } else if (dif < slide) {
+      e.preventDefault();
+
+      // Crear un nuevo div para mostrar el mensaje de error
+      var mensajeError = document.createElement("div");
+      mensajeError.classList.add("invalid-feedback");
+      mensajeError.innerHTML =
+        "No puede haber menos numeros que espacios del vector";
+
+      // Agregar el nuevo div de error después del campo "minimo"
       minimo.parentNode.insertBefore(
         mensajeError,
         minimo.nextSibling
