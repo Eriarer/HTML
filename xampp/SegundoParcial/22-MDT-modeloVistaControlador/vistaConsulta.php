daab46dbae6d7be36023470a30fa1746e1901265
Eri <103050349+Eriarer@users.noreply.github.com>
2023-10-13 16:19:35 -0600

    alert danger

--- xampp/SegundoParcial/22-MDT-modeloVistaControlador/vistaConsulta.php
+++ xampp/SegundoParcial/22-MDT-modeloVistaControlador/vistaConsulta.php
@@ -29,11 +29,9 @@
   {
   ?>
     <div style="display:flex;justify-content:center;padding-top:50px;">
-      <div style="width: 25%; " class="alert alert-warning alert-dismissible fade show" role="alert">
+      <div style="width: 25%; " class="alert alert-danger fade show" role="alert">
         <strong>Empleado: <?php echo $id ?></strong> <br> No registrado.
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+
       </div>
     </div>
   <?php
