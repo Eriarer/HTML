ab294152544b3b2bf3e59653675b43857fa59dee
Eri <103050349+Eriarer@users.noreply.github.com>
2023-10-18 19:21:10 -0600

    fileReaderClass almost done

--- xampp/SegundoParcial/26-MDT-usuarios/fileReader.php
+++ xampp/SegundoParcial/26-MDT-usuarios/fileReader.php
@@ -3,9 +3,12 @@ class FileReader
 {
   private $filePath;
   private $handle;
-
-  private $currentLine = 0;
-  private $wordCount = 0;
+  private $currentLine = 1;
+  private $currentColumn = 1;
+  private $currentWord = 0;
+  private $currentChar = 0;
+  private $newLine = false;
+  private $newWord = true;
 
   public function __construct($filePath = null)
   {
@@ -39,6 +42,26 @@ class FileReader
     return $this->filePath;
   }
 
+  public function getCurrentLine()
+  {
+    return $this->currentLine;
+  }
+
+  public function getCurrentColumn()
+  {
+    return $this->currentColumn;
+  }
+
+  public function getCurrentWord()
+  {
+    return $this->currentWord;
+  }
+
+  public function getCurrentChar()
+  {
+    return $this->currentChar;
+  }
+
   public function fileExists(): bool
   {
     if ($this->filePath === null)
@@ -61,8 +84,10 @@ class FileReader
       throw new Exception("No se pudo abrir el archivo");
     }
 
-    $this->currentLine = 0;
-    $this->wordCount = 0;
+    $this->currentLine = 1;
+    $this->currentColumn = 1;
+    $this->currentWord = 0;
+    $this->currentChar = 0;
   }
 
   public function closeFile()
@@ -74,68 +99,111 @@ class FileReader
     }
   }
 
-  /**
-   * Lee una línea del archivo.
-   * @return string|null La línea leída o null si llegamos al final del archivo.
-   */
-  public function readLine(): ?string
+  public function readChar()
   {
     if ($this->handle === null)
     {
       $this->openFile();
     }
 
-    $line = fgets($this->handle);
-    if ($line !== false)
+    if ($this->newLine)
     {
+      $this->newLine = false;
       $this->currentLine++;
-      $this->wordCount += str_word_count($line);
+      $this->currentColumn = 1;
     }
 
-    return $line;
+    $char = fgetc($this->handle);
+
+    if ($char !== false)
+    {
+      $this->currentChar++;
+      if ($char === "\n")
+      {
+        $this->newLine = true;
+        $this->newWord = true;
+      }
+      else if ($char === ' ' || $char === "\t")
+      {
+        $this->newWord = true;
+        $this->currentColumn++;
+      }
+      else
+      {
+        if ($this->newWord)
+        {
+          $this->newWord = false;
+          $this->currentWord++;
+        }
+        $this->currentColumn++;
+      }
+    }
+
+    return $char;
   }
 
-  /**
-   * Lee una palabra del archivo.
-   * @return string|null La palabra leída o null si llegamos al final del archivo.
-   */
-  public function readWord(): ?string
+  public function readWord()
   {
-    if ($this->handle === null)
-    {
-      $this->openFile();
-    }
-
     $word = '';
-    $char = fread($this->handle, 1);
-
-    while ($char !== false)
+    while (($char = $this->readChar()) !== false)
     {
       if ($char === ' ' || $char === "\n" || $char === "\t")
       {
         if ($word !== '')
         {
-          $this->wordCount++;
           return $word;
-        }
-        if ($char === "\n")
-        {
-          $this->currentLine++;
         }
       }
       else
       {
         $word .= $char;
       }
-      $char = fread($this->handle, 1);
     }
-
-    if ($word !== '')
-    {
-      $this->wordCount++;
-      return $word;
-    }
-
-    return null;
+    return ($word !== '') ? $word : null;
   }
+
+  public function readLine()
+  {
+    $line = '';
+    while (($char = $this->readChar()) !== false)
+    {
+      $line .= $char;
+      if ($char === "\n")
+      {
+        return $line;
+      }
+    }
+    return ($line !== '') ? $line : null;
+  }
+
+  public function resetCursor(int $line, int $column)
+  {
+    if ($this->handle === null)
+    {
+      $this->openFile();
+    }
+
+    //resetar el handle a la posicion 0
+    fseek($this->handle, 0);
+    $this->currentLine = 1;
+    $this->currentColumn = 1;
+    $this->currentWord = 0;
+    $this->currentChar = 0;
+    $this->newLine = false;
+    $this->newWord = true;
+  }
+
+  /*public function setCursor(){
+    //  Casos
+    //  -1 -1 -> ir al final del archivo
+    //  -1 0 -> Ultima linea del archivo primer posicion del cursor en esa linea
+    //  -1 m -> Ultima linea del archivo posicion del cursor en la columna m, si m es mayor que las numeros de columnas aplicar logica como si n = -1
+    //  0 -1 -> Primera linea del archivo ultima columna del archivo
+    //  0 0 -> Inicio del archivo
+    //  0 m -> Primera linea del archivo posicion del cursor en la columna m, si m es mayor que las numeros de columnas aplicar logica como si n = -1
+    //  n -1 -> Linea n del archivo ultima columna del archivo, si n es mayor que las numeros de lineas aplicar logica como si n = -1
+    //  n 0 -> Linea n del archivo primera columna del archivo, si n es mayor que las numeros de lineas aplicar logica como si n = -1
+    //  n m -> Linea n del archivo columna n del archivo, si n es mayor que las numeros de lineas aplicar logica como si n = -1, si m es mayor que las numeros de columnas aplicar logica como si n = -1
+  }
+  */
 }
