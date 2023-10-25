caed05c61681d91d374cb2cd546f1d3208e7fd29
Eri <103050349+Eriarer@users.noreply.github.com>
2023-10-25 14:57:24 -0600

    alguna actividad

---
+++ xampp/SegundoParcial/27-MDT-sesiones/registro.php
@@ -0,0 +1,28 @@
+<body>
+    
+
+<?php
+ 
+require 'formulario.php';
+
+?>
+<div style="margin:100px;">
+<?php
+     
+ 
+    $usuario = $_POST["login"];
+    $contra = $_POST["contra"];
+     
+    $file = fopen("archivo.txt","a+");
+     
+    fwrite($file, $usuario." ".$contra."\r\n");
+    
+    fclose($file);
+    echo "<br> cuenta insertada <br>";   
+     
+    echo "<br><a href='formulario.php'>regresar</a>";
+
+?>
+</div>
+</body>
+
